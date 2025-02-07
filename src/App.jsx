b8e1db41c67bc48d2e96522fdf48e660b331ae25
Abir Chodha <abirchodha1@gmail.com>
2025-02-08 04:14:04 +0530

    Create 404 Not Found page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import RootLayout from "./layout/RootLayout";
 import ContactLayout from "./layout/ContactLayout";
 import ContactInfo from "./components/ContactInfo";
 import ContactForm from "./components/ContactForm";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="info" element={<ContactInfo />} />
           <Route path="form" element={<ContactForm />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Route>
     )
   );
