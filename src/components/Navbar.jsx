aa2187f3472663021d52d625bcf4cb7566c8d2cb
Abir Chodha <abirchodha1@gmail.com>
2025-02-08 03:02:11 +0530

    Make all li elements as Link and define their path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <img src={logo} alt="logo" />
       <ul>
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/products">
+          <li>Products</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
       </ul>
       <button>Get Started</button>
     </div>
